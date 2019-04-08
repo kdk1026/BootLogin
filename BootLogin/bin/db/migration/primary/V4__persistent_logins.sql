2ad958891a0a6d13251ce954de3d996f2175567f
kdk1026 <kdk1026@gmail.com>
2019-04-09 00:39:12 +0900

    bin 제거

--- BootLogin/bin/db/migration/primary/V4__persistent_logins.sql
+++
@@ -1,8 +0,0 @@
---자동로그인
-CREATE TABLE persistent_logins (
-	username 		VARCHAR(64) 	NOT NULL,			--아이디
-    series 			VARCHAR(64) 	NOT NULL,			--
-    token 			VARCHAR(64) 	NOT NULL,			--자동로그인 토큰
-    last_used 		DATETIME 		DEFAULT SYSDATE,	--
-    PRIMARY KEY (series)		
-);
