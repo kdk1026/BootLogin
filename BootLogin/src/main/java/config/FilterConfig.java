ed2832fcfdd100fd8e3ea00e760e090e1b071ff2
kdk <kdk1026@gmail.com>
2021-05-13 22:55:55 +0900

    수정 및 그레줄 버전 주석 명시

--- BootLogin/src/main/java/config/FilterConfig.java
+++ BootLogin/src/main/java/config/FilterConfig.java
@@ -1,11 +1,5 @@
 package config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import common.servlet.filter.SecurityFilter;
-
 /**
  * @since 2019. 1. 1.
  * @author 김대광
@@ -15,18 +9,18 @@ import common.servlet.filter.SecurityFilter;
  * 2019. 1. 1. 김대광	최초작성
  * </pre>
  */
-@Configuration
-public class FilterConfig {
-
-	@Bean
-	public FilterRegistrationBean getSecurityFilterRegistrationBean() {
-		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-		
-		registrationBean.setFilter( new SecurityFilter() );
-		registrationBean.addUrlPatterns("/*");
-		
-		return registrationBean;
-	}
-	
-	
-}
+//@Configuration
+//public class FilterConfig {
+//
+//	@Bean
+//	public FilterRegistrationBean getSecurityFilterRegistrationBean() {
+//		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+//		
+//		registrationBean.setFilter( new SecurityFilter() );
+//		registrationBean.addUrlPatterns("/*");
+//		
+//		return registrationBean;
+//	}
+//	
+//	
+//}
